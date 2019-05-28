77f6ad4861795678800f8db5b41ec2d9ec936a2d
Mary Krause <mariekrause@Dresdens-MacBook-Pro.local>
2019-05-28 12:19:27 +0200

    added agb/datenschutz

--- webpage/script.js
+++ webpage/script.js
@@ -6,3 +6,8 @@ function parallax() {
     var scrolled = $(window).scrollTop();
     $('.background').css('background-position-y', -(Math.abs(scrolled) * 0.025) + 'rem');
 };
+
+function show_hide() {
+    document.getElementById("datenschutz").classList.toggle("visible")
+
+}
